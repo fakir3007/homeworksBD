df9a1873ee8a4b9b85058a79b87011929d53e890
fakir3007 <91556351+fakir3007@users.noreply.github.com>
2022-03-14 18:06:14 +0500

    code changes

--- скрипт 2.sql
+++ скрипт 2.sql
@@ -1,36 +1,45 @@
 create table if not exists genre(
 	id serial primary key,
-	name varchar(40),
-	perfomers_id integer not null references perfomers(id) 
+	name varchar(40)
 );
 
 create table if not exists perfomers(
 	id serial primary key,
-	name varchar(40),
-	genre_id integer not null references genre(id),
-	alboms_id integer not null references alboms(id),
-	tracks_id integer not null references tracks(id) 
+	name varchar(40)
 );
 
+CREATE TABLE IF NOT EXISTS PerfomersGenre(
+	id serial primary key,
+	genre_id integer REFERENCES genre(id),
+	perfomers_id integer REFERENCES perfomers(id)
+);
 create table if not exists alboms(
 	id serial primary key,
 	name varchar(40),
-	year integer,
-	perfomers_id integer not null references perfomers(id),
-	tracks_id integer not null references tracks(id) 
+	year integer 
+);
+
+CREATE TABLE IF NOT EXISTS PerfomersAlboms(
+	id serial primary key,
+	perfomers_id integer REFERENCES perfomers(id),
+	alboms_id integer REFERENCES alboms(id)
 );
 
 create table if not exists tracks(
 	id serial primary key,
 	name varchar(40),
-	duration numeric,
-	perfomers_id integer not null references perfomers(id)
+	duration numeric
 );
 
 create table if not exists compilation(
 	id serial primary key,
 	name varchar(40),
-	duration numeric,
-	tracks_id integer not null references tracks(id)
+	duration numeric
+);
+
+CREATE TABLE IF NOT EXISTS TracksComplation(
+	id serial primary key,
+	tracks_id integer REFERENCES tracks(id),
+	compilation_id integer REFERENCES compilation(id)
 );
 
